344f297f7577cd9c5d15734aa3934a29d3882453
Alex Lobov <lexlobov@yandex.ru>
2022-05-09 19:42:22 +0700

    Добавил аннотации аллюра. Небольшие правки

--- src/test/java/AccountTest.java
+++ src/test/java/AccountTest.java
@@ -1,3 +1,5 @@
+import io.qameta.allure.Description;
+import io.qameta.allure.junit4.DisplayName;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
@@ -5,6 +7,7 @@ import org.junit.runners.Parameterized;
 import static org.junit.Assert.assertEquals;
 
 @RunWith(Parameterized.class)
+@DisplayName("Параметризованный тест-сьют на проверку метода checkNameToEmboss")
 public class AccountTest {
 
     private String name;
@@ -18,28 +21,43 @@ public class AccountTest {
     @Parameterized.Parameters
     public static Object[][] getData(){
         return new Object[][] {
-                {"John Cena",   true},
-                {" John Cena",  false},
-                {"John Cena ",  false},
-                {" John Cena ", false},
-                {"JohnCena",    false},
-                {"John C ena",  false},
-                {"JohnCena ",   false},
-                {" JohnCena",   false},
-                {"JohnCena ",   false},
-                {"Benedict superCumberbatch", false},
-                {"Benedict Cumberbatch", false},
-                {"Benedict Cumberbatc", true},
-                {"Benedict Cumberbat", true},
-                {"A B", true},
-                {"AB", false},
-                {"A Ba", true}
+                {"John Cena",                   true},
+                {" John Cena",                  false},
+                {"John Cena ",                  false},
+                {" John Cena ",                 false},
+                {"JohnCena",                    false},
+                {"John C ena",                  false},
+                {"JohnCena ",                   false},
+                {" JohnCena",                   false},
+                {"Benedict superCumberbatch",   false},
+                {"Benedict Cumberbatch",        false},
+                {"Benedict Cumberbatc",         true},
+                {"Benedict Cumberbat",          true},
+                {"A B",                         true},
+                {"AB",                          false},
+                {"A Ba",                        true}
         };
     }
     @Test
+    @DisplayName("Проверка метода с различными входными параметрами")
+    @Description("Проверяются:" +
+            "позитивный сценарий; " +
+            "сценарий с пробелом в началае и посередине; " +
+            "сценарий с пробелом в конце; " +
+            "сценарий с пробелом в начале, посередине и конце; " +
+            "сценарий без пробела; " +
+            "сценарий с двумя пробелами посередине; " +
+            "сценарий с пробелом в конце без пробела посередине; " +
+            "сценарий с пробелом в начале без пробела посередине; " +
+            "сценарий с более чем 19 символами; " +
+            "сценарий с 20 символами; " +
+            "сценрарий с 19 символами; " +
+            "сценарий с 18 символами; " +
+            "сценарий с 3 символами; " +
+            "сценарий с 2 символами; " +
+            "сценарий с 4 сиволами. ")
     public void testCheckNameToEmboss(){
         Account account = new Account(name);
-        System.out.println(name);
         assertEquals(result, account.checkNameToEmboss());
     }
 }
