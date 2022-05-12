8ef3d326e7d4b2cb4b69edd27ff6dd15e8636931
Alex Lobov <lexlobov@yandex.ru>
2022-05-12 20:01:19 +0700

    Дописал появснение к методу

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -7,9 +7,17 @@ public class Account {
     }
 
     public boolean checkNameToEmboss() {
-        if (name.startsWith(" ") | name.endsWith(" ") | !name.contains(" ")){
+        if (name.startsWith(" ")){
             return false;
-        } else if (!(name.indexOf(" ") == name.lastIndexOf(" "))){
+        } else if(!name.contains(" ")) {
+            return false;
+        } else  if (name.endsWith(" ")){
+            return  false;
+            // В коде ниже проверяется, что первый в строке индекс, по которому встречается
+            // пробел, равен последнему в строке индексу, по которому есть пробел.
+            //Таким образом алгоритм находит первый пробел от начала строки, затем находит первый от конца строки.
+            //Если их индексы совпадают, то получается, что это один и тот же пробел, следовательно других в строке нет.
+        }else if (!(name.indexOf(" ") == name.lastIndexOf(" "))){
             return false;
         }else if(name.length() < 3 | name.length()>19){
             return false;
